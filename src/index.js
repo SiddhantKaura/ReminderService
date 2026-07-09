4a81bfa79186a1742ac30c5e254de45f8154b68c
Siddhant Kaura <siddhantkaura@Siddhants-Air.bbrouter>
2026-07-09 23:16:50 +0530

    chore: starting with cron

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { PORT } = require("./config/serverConfig");
 const { sendBasicEmail } = require("./services/email-service");
+const cron = require("node-cron");
 
 const app = express();
 app.use(express.json());
@@ -8,10 +9,14 @@ app.use(express.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
   console.log("Server started at ", PORT);
-  sendBasicEmail(
-    "support@admin.com",
-    "skanonymous001@gmail.com",
-    "This is a test email",
-    "Hi There",
-  );
+  //   sendBasicEmail(
+  //     "support@admin.com",
+  //     "skanonymous001@gmail.com",
+  //     "This is a test email",
+  //     "Hi There",
+  //   );
+
+  cron.schedule("* * * * *", () => {
+    console.log("running a task every minute");
+  });
 });
